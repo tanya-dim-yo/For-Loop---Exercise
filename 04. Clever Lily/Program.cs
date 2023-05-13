db48e05a791099fc43ae303abb6d409ab0f9a461
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-13 05:23:01 +0300

    Add files via upload

---
+++ 04. Clever Lily/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CleverLilly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+            int savedMoney = 0;
+            int moneyForBirthday = 10;
+            int cntToys = 0;
+
+            for (int currentYear = 1; currentYear <= age; currentYear++)
+            {
+                if (currentYear % 2 == 0)
+                {
+                    savedMoney += (moneyForBirthday - 1);
+                    moneyForBirthday += 10;
+                }
+                else
+                {
+                    cntToys++;
+                }
+            }
+
+            savedMoney += cntToys * toyPrice;
+
+            Console.WriteLine(savedMoney >= washingMachinePrice ? $"Yes! {(savedMoney - washingMachinePrice):0.00}" : $"No! {(washingMachinePrice - savedMoney):0.00}");
+        }
+    }
+}
