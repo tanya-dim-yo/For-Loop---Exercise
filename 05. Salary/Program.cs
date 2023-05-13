6ab5b60e6642edf6faa0f4d0ce2ec1353139227f
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-13 05:24:07 +0300

    Add files via upload

---
+++ 05. Salary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openedTabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= openedTabs; i++)
+            {
+                string website = Console.ReadLine();
+
+                if (website == "Facebook")
+                {
+                    salary -= 150.0;
+                }
+                else if (website == "Instagram")
+                {
+                    salary -= 100.0;
+                }
+                else if (website == "Reddit")
+                {
+                    salary -= 50.0;
+                }
+
+                if (salary <= 0)
+                {
+                    break;
+                }
+            }
+
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(Math.Round(salary));
+            }
+        }
+    }
+}
