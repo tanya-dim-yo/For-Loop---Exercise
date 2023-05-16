04e7abc28aba54aebffa94e3dab258e77e97d24d
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-16 04:14:27 +0300

    Add files via upload

---
+++ 08. Tennis Ranklist/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TennisRanklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberTournaments = double.Parse(Console.ReadLine());
+            int totalPoints = int.Parse(Console.ReadLine());
+            double pointsTournaments = 0;
+            double tournamentsWinner = 0;
+
+            for (int i = 1; i <= numberTournaments; i++)
+            {
+                string level = Console.ReadLine();
+
+                if (level == "SF")
+                {
+                    totalPoints += 720;
+                    pointsTournaments += 720;
+                }
+                else if (level == "F")
+                {
+                    totalPoints += 1200;
+                    pointsTournaments += 1200;
+                }
+                else if (level == "W")
+                {
+                    totalPoints += 2000;
+                    pointsTournaments += 2000;
+                    tournamentsWinner++;
+                }
+            }
+
+            double averagePoints = Math.Floor(pointsTournaments / numberTournaments);
+            double percentWinner = (tournamentsWinner * 100.0) / numberTournaments;
+
+            Console.WriteLine($"Final points: {totalPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{percentWinner:F2}%");
+        }
+    }
+}
